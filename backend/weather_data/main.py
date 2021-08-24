09ad836d41c98fa0374f45af4858f9dd7e1bfdd3
ChangEn Yea <nsce9806q@gmail.com>
2021-08-24 17:50:09 +0900

    add 날씨 데이터 스케쥴링

--- backend/weather_data/main.py
+++ backend/weather_data/main.py
@@ -1,1 +1,24 @@
-print("hello world")
+import asyncio, time, schedule
+
+# 날씨 정보 조회 및 db 저장 모듈
+from now import setWeatherNow
+
+'''
+특정 시간마다 http 통신을 이용하여 redis에 날씨 정보 저장
+'''
+
+# 매 시간 마다
+def job_every_hour():
+    setWeatherNow()
+# 매 자정 마다
+def job_every_day():
+    pass
+
+'''
+스케쥴 항목
+'''
+schedule.every().hour.at(":00").do(job_every_hour)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
